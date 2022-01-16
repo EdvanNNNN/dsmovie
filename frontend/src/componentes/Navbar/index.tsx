22205d55c69cb6bb98d87abf1c3684ff16d6169e
Eddlp <edvan125lima@gmail.com>
2022-01-16 04:25:44 -0300

    correção de erros

--- frontend/src/componentes/Navbar/index.tsx
+++ frontend/src/componentes/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
           <header>
             <nav className="container">
               <div className="dsmovie-nav-content">
-                <h1>Sla</h1>
+                <h1>Gmovie</h1>
                 <a href="https://github.com/EdvanNNNN">
                   <div className="dsmovie-contact-container">
                     <GithubIcon />
